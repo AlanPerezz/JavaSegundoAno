f62d3bf71b95d78b6143d692cce30eade73af68f
AlanPerezz <47872161+AlanPerezz@users.noreply.github.com>
2019-03-21 19:42:41 -0300

    novo cartao

--- Cartao.java
+++ Cartao.java
@@ -1,17 +1,33 @@
 public class Cartao {
-  float saldo;
-  String titular;
+	private float saldo;
+	private String titular;
+	
+	public Cartao(String t) {
+		titular = t;
+	}
+
+	public void creditar(float paramFloat) {
+		saldo += paramFloat;
+	}
   
-  public Cartao() {}
-  
-  void creditar(float paramFloat) { saldo += paramFloat; }
-  
-  boolean debitar(float paramFloat)
-  {
-    if (saldo >= paramFloat) {
-      saldo -= paramFloat;
-      return true;
-    }
-    return false;
-  }
+	public boolean debitar(float paramFloat) {
+		if (saldo >= paramFloat) {
+			saldo -= paramFloat;
+			return true;
+		}
+
+		return false;
+	}
+	
+	public float getSaldo() {
+		return saldo;
+	}
+	
+	public void setTitular(String t) {
+		titular = t;
+	}
+	
+	public String getTitular() {
+		return titular;
+	}
 }
